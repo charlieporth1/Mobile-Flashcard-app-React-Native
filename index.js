7a95b1c54f6bf908569da1348c646ff949361170
Charlie Porth <charlieporth1@gmail.com>
2021-03-01 19:41:55 -0600

    Its working don't touch it

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { AppRegistry } from 'react-native';
-
 import App from './src/App';
+import {registerRootComponent} from "expo";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-AppRegistry.registerComponent('main', () => App);
+// AppRegistry.registerComponent('main', () => App);
+registerRootComponent(App);
